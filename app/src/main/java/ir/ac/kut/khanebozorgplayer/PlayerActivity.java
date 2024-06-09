33902a1c664f711857ecdc9f6e2f454872e70f4c
Shahriar <shahriar.hd@outlook.com>
2024-06-09 18:17:10 +0330

    Part 5, seekbar and duration, TODO. June 9 2024 18:16

--- app/src/main/java/ir/ac/kut/khanebozorgplayer/PlayerActivity.java
+++ app/src/main/java/ir/ac/kut/khanebozorgplayer/PlayerActivity.java
@@ -1,6 +1,14 @@
 package ir.ac.kut.khanebozorgplayer;
 
+import static ir.ac.kut.khanebozorgplayer.MainActivity.audioFiles;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,108 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
 public class PlayerActivity extends AppCompatActivity {
 
+    TextView song_name, artist_name, duration_played, duration_total;
+    ImageView cover_art, next_botton, previos_button, back_button, shuffle_button, reapeat_button;
+    FloatingActionButton play_button;
+    SeekBar seekBar;
+    int position = -1;
+    static ArrayList<AudioFiles> listSongs = new ArrayList<>();
+    static Uri uri;
+    static MediaPlayer mediaPlayer;
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_player);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.player_container), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        initViews();
+        getIntentMethod();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mediaPlayer != null && fromUser) {
+                    mediaPlayer.seekTo(progress * 1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
         });
+        PlayerActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mediaPlayer != null) {
+
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition() / 1000);
+                    //TODO: set duration
+                    //duration_played.setText(formattedTime(mediaPlayer.getCurrentPosition() / 1000));
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    private String formattedTime(int aCurrentPosition) {
+        String totalout = "";
+        String totalNew = "";
+        String seconds = String.valueOf(aCurrentPosition % 60);
+        String minutes = String.valueOf(aCurrentPosition / 60);
+        totalout = minutes + ":" + seconds;
+        totalNew = minutes + ":" + "0" + seconds;
+        if (seconds.length() == 1) {
+            return totalNew;
+        } else {
+            return totalout;
+        }
+    }
+
+    private void getIntentMethod() {
+        position = getIntent().getIntExtra("position", -1);
+        listSongs = audioFiles;
+        if (listSongs != null) {
+            play_button.setImageResource(R.drawable.icon_pause);
+            uri = Uri.parse(listSongs.get(position).getPath());
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+
+        }
+        else {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+        }
+        seekBar.setMax(mediaPlayer.getDuration() / 1000);
+
+    }
+
+    private void initViews() {
+        song_name = findViewById(R.id.song_name);
+        artist_name = findViewById(R.id.song_artist);
+        cover_art = findViewById(R.id.cover_art);
+        next_botton = findViewById(R.id.next);
+        previos_button = findViewById(R.id.previous);
+        back_button = findViewById(R.id.back_botton);
+        shuffle_button = findViewById(R.id.shuffle);
+        reapeat_button = findViewById(R.id.repeat);
+        play_button = findViewById(R.id.play_puase);
+        seekBar = findViewById(R.id.seek_bar);
+
+
     }
 }

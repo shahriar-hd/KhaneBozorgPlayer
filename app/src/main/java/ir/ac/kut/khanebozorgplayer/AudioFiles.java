9a7be829b8244f1fd2362e52f5ee43de926fb5fe
Shahriar <shahriar.hd@outlook.com>
2024-06-10 13:48:29 +0330

    Part 6, delete file, not complete, see TODO in AudioAdapter. June 10 2024 13:48

--- app/src/main/java/ir/ac/kut/khanebozorgplayer/AudioFiles.java
+++ app/src/main/java/ir/ac/kut/khanebozorgplayer/AudioFiles.java
@@ -1,14 +1,15 @@
 package ir.ac.kut.khanebozorgplayer;
 
 public class AudioFiles {
-    private String path, title, artist, album, duration;
+    private String path, title, artist, album, duration, id;
 
-    public AudioFiles(String path, String title, String artist, String album, String duration) {
+    public AudioFiles(String path, String title, String artist, String album, String duration, String id) {
         this.path = path;
         this.title = title;
         this.artist = artist;
         this.album = album;
         this.duration = duration;
+        this.id = id;
     }
 
     public AudioFiles() {
@@ -54,4 +55,11 @@ public class AudioFiles {
     public void setDuration(String duration) {
         this.duration = duration;
     }
+
+    public String getID() {
+    return id;
 }
+
+    public void setID(String id) {this.id = id;}
+}
+
